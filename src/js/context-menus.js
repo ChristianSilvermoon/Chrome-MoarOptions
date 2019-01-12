b937d9852dbd1b8743f86cd0bf97f9b2888184ce
Christian "Krissy" Silvermoon <ChristianSilvermoon@users.noreply.github.com>
2019-01-11 22:48:59 -0700

    Added Copy Address Without Paramters

--- src/js/context-menus.js
+++ src/js/context-menus.js
@@ -1,8 +1,4 @@
 
-function wipMessage() {
-	alert("Still Getting To That...");
-}
-
 const ImageMenu = chrome.contextMenus.create({
 	"id": "viewImageMenu",
 	"title": "View Image",
@@ -15,13 +11,36 @@ const videoMenu = chrome.contextMenus.create({
 	"contexts": [ "video" ]
 })
 
+const copyWithoutParametersMenu = chrome.contextMenus.create({
+	"id": "copyURLwithoutParameters",
+	"title": "Copy Current Address Without Parameters",
+	"contexts": [ "page" ]
+})
+
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
 	switch(info.menuItemId) {
 		case "viewImageMenu":
 		case "viewVideoMenu":
         		chrome.tabs.update(tab.id, {url: info.srcUrl});
 			break;
+
+		case "copyURLwithoutParameters":
+			console.log(tab);
+			copyTextToClipboard(tab.url.split("?")[0]);
+			break;
 		default:
 			break;
 	}
 });
+
+
+function copyTextToClipboard(text) {
+  var copyFrom = document.createElement("textarea");
+  copyFrom.textContent = text;
+
+  document.body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy');
+  copyFrom.blur();
+  document.body.removeChild(copyFrom);
+}
